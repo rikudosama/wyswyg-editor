b291e2e9aeec67f89ea61ccb83da9f8274372846
lengam bonaveneture <bjean633@gmail.com>
2017-07-01 18:08:25 +0000

    creation models and polymophique relationship

---
+++ app/Attachment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attachment extends Model
+{
+    protected $guarded = [];
+    public function attachable()
+    {
+        $this->morphTo();
+    }
+}
